20deec39f8eff634ce0f814bc2bc8bb5b723c9cf
ada10mci <ada10mci@student.lth.se>
2014-11-13 13:49:15 +0100

    my second commit

--- src/MainGUI.java
+++ src/MainGUI.java
@@ -1,6 +1,8 @@
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.Font;
 
 
 public class MainGUI {
@@ -35,8 +37,28 @@ public class MainGUI {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 664, 474);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("State matrix (A): ");
+		lblNewLabel.setFont(new Font("Dialog", Font.PLAIN, 12));
+		lblNewLabel.setBounds(33, 59, 164, 15);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Input matrix (B): ");
+		lblNewLabel_1.setFont(new Font("Dialog", Font.PLAIN, 12));
+		lblNewLabel_1.setBounds(33, 89, 164, 15);
+		frame.getContentPane().add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("Output matrix (C): ");
+		lblNewLabel_2.setFont(new Font("Dialog", Font.PLAIN, 12));
+		lblNewLabel_2.setBounds(33, 116, 164, 15);
+		frame.getContentPane().add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("Feedthrough matrix (D): ");
+		lblNewLabel_3.setFont(new Font("Dialog", Font.PLAIN, 12));
+		lblNewLabel_3.setBounds(33, 143, 164, 15);
+		frame.getContentPane().add(lblNewLabel_3);
 	}
-
 }
