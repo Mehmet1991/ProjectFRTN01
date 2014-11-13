3b0e98c57434ec5771de2cafc25e94afca061b27
ada10mci <ada10mci@student.lth.se>
2014-11-13 13:54:27 +0100

    my third commit

--- src/MainGUI.java
+++ src/MainGUI.java
@@ -3,11 +3,16 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import java.awt.Font;
+import javax.swing.JTextField;
 
 
 public class MainGUI {
 
 	private JFrame frame;
+	private JTextField txtA;
+	private JTextField txtB;
+	private JTextField txtC;
+	private JTextField txtD;
 
 	/**
 	 * Launch the application.
@@ -60,5 +65,25 @@ public class MainGUI {
 		lblNewLabel_3.setFont(new Font("Dialog", Font.PLAIN, 12));
 		lblNewLabel_3.setBounds(33, 143, 164, 15);
 		frame.getContentPane().add(lblNewLabel_3);
+		
+		txtA = new JTextField();
+		txtA.setBounds(215, 57, 146, 19);
+		frame.getContentPane().add(txtA);
+		txtA.setColumns(10);
+		
+		txtB = new JTextField();
+		txtB.setBounds(215, 87, 146, 19);
+		frame.getContentPane().add(txtB);
+		txtB.setColumns(10);
+		
+		txtC = new JTextField();
+		txtC.setBounds(215, 114, 146, 19);
+		frame.getContentPane().add(txtC);
+		txtC.setColumns(10);
+		
+		txtD = new JTextField();
+		txtD.setBounds(215, 141, 146, 19);
+		frame.getContentPane().add(txtD);
+		txtD.setColumns(10);
 	}
 }
